5da7231b1cfc99198987f55189e1f2f4e760e832
thatbeer <88705394+thatbeer@users.noreply.github.com>
2022-10-04 00:06:09 +0700

    add dependecies package

---
+++ webapp/src/route/navigator/navigator.route.jsx
@@ -0,0 +1,13 @@
+import { Link  } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+
+const navigator = () => {
+    return (
+        <Fragment>
+            <div>
+                I am the Navigator
+            </div>
+        </Fragment>
+    )
+}
